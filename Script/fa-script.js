c8a5485d4f56c701e225e25cb857413dc697af90
Kiiarash49 <kiarash.soltani87@gmail.com>
2024-08-03 00:07:42 +0430

    No new information

--- Script/fa-script.js
+++ Script/fa-script.js
@@ -1,7 +1,3 @@
-document.getElementById("register").addEventListener("click", function () {
-    alert("در حال حاظر ثبت نام در دسترس نیست")
-})
-
 document.getElementById("about").addEventListener("click", function () {
     alert("در حال حاظر این صفحه در دسترس نیست")
 })
