2d8fd16ef8f897b467f076e8adfbbc334682ec46
Kiiarash49 <kiarash.soltani87@gmail.com>
2024-08-02 22:56:04 +0430

    Bugs fixed

--- Script/en-script.js
+++ Script/en-script.js
@@ -1,7 +1,7 @@
-document.getElementById("register").addEventListener("click", function() {
+document.getElementById("register").addEventListener("click", function () {
     alert("Registration is currently unavailable")
 })
 
-document.getElementById("about").addEventListener("click", function() {
+document.getElementById("about").addEventListener("click", function () {
     alert("This page is currently unavailable")
 })
--- Script/fa-script.js
+++ Script/fa-script.js
@@ -1,7 +1,7 @@
-document.getElementById("register").addEventListener("click", function() {
+document.getElementById("register").addEventListener("click", function () {
     alert("در حال حاظر ثبت نام در دسترس نیست")
 })
 
-document.getElementById("about").addEventListener("click", function() {
+document.getElementById("about").addEventListener("click", function () {
     alert("در حال حاظر این صفحه در دسترس نیست")
 })
