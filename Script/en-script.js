95f223e727b6d46fe14456f7ee9da31385430585
Kiiarash49 <kiarash.soltani87@gmail.com>
2024-08-03 14:35:09 +0430

    Now you can register in persian and english language

--- Script/en-script.js
+++ Script/en-script.js
@@ -1,7 +1,3 @@
-document.getElementById("register").addEventListener("click", function () {
-    alert("Registration is currently unavailable")
-})
-
 document.getElementById("about").addEventListener("click", function () {
     alert("This page is currently unavailable")
 })
